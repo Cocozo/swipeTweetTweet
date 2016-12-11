30f472511807f2de6444734e5a11fa85df4c4fb2
김재하 <kjh062627@naver.com>
2016-12-11 20:40:50 +0900

    트윗트윗 프로젝트 생성

---
+++ SwipeTweetTweet/SwipeTweetTweet/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwipeTweetTweet
+//
+//  Created by 김재하 on 2016. 12. 11..
+//  Copyright © 2016년 Pabix. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwipeTweetTweet/SwipeTweetTweet/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwipeTweetTweet
+//
+//  Created by 김재하 on 2016. 12. 11..
+//  Copyright © 2016년 Pabix. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
